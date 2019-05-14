a147d8a4f16edc8a59477f812cad9b5d35d4ec1c
marcus <mingyen1202@gmail.com>
2019-05-14 16:15:08 +0800

    Add add customer

---
+++ src/main/java/com/example/springBootJPADemo/CustomerForm.java
@@ -0,0 +1,18 @@
+package com.example.springBootJPADemo;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Data
+public class CustomerForm {
+
+    @NotNull
+    @Size(min = 1, max = 64)
+    private String firstName;
+
+    @NotNull
+    @Size(min = 1, max = 64)
+    private String lastName;
+}
--- src/main/java/com/example/springBootJPADemo/controller/CustomerContorller.java
+++ src/main/java/com/example/springBootJPADemo/controller/CustomerContorller.java
@@ -1,13 +1,18 @@
 package com.example.springBootJPADemo.controller;
 
+import com.example.springBootJPADemo.CustomerForm;
 import com.example.springBootJPADemo.model.Customer;
 import com.example.springBootJPADemo.service.CustomerService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -21,6 +26,34 @@ public class CustomerContorller {
         List<Customer> customers = service.findAll();
         model.addAttribute("customers", customers);
         return "customers/list";
+    }
+
+    @ModelAttribute
+    private CustomerForm setupForm() {
+        return new CustomerForm();
+    }
+
+    @RequestMapping(value = "create", method = RequestMethod.POST)
+    private String create(@Valid CustomerForm form, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            return listAll(model);
+        }
+        Customer c = new Customer();
+        BeanUtils.copyProperties(form, c);
+        c.setAge(18);
+        service.create(c);
+        return "redirect:/customers";
+    }
+
+    @RequestMapping(value = "delete", method = RequestMethod.POST)
+    private String delete(@Valid CustomerForm form, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            return listAll(model);
+        }
+        Customer c = new Customer();
+        BeanUtils.copyProperties(form, c);
+        service.delete(c.getId());
+        return "redirect:/customers";
     }
 
 }
