e3075546aa1e235ed8346f3fd5911396033b8daa
marcus <mingyen1202@gmail.com>
2019-05-14 14:44:58 +0800

    Add Swagger2

--- src/main/java/com/example/springBootJPADemo/SpringBootJpaDemoApplication.java
+++ src/main/java/com/example/springBootJPADemo/SpringBootJpaDemoApplication.java
@@ -3,9 +3,11 @@ package com.example.springBootJPADemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableSwagger2
 public class SpringBootJpaDemoApplication {
 
 	public static void main(String[] args) {
